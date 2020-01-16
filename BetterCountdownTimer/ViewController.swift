2da129e78b49f675dfa0e014d8c3024f6846e0f0
Billy <williamvrossii@gmail.com>
2020-01-16 12:20:53 +0000

    Initial Commit

---
+++ BetterCountdownTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BetterCountdownTimer
+//
+//  Created by Billy Sumners on 16/01/2020.
+//  Copyright © 2020 Billy Sumners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
