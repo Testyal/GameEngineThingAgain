eb23cb6fc756e4df0f5daf42684f92279e0a91e0
Billy <williamvrossii@gmail.com>
2020-02-26 14:21:25 +0000

    Deleted text I completely forgot about

--- GameEngine/src/Logic/Logic.swift
+++ GameEngine/src/Logic/Logic.swift
@@ -115,22 +115,3 @@ class InputParser {
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/*
-Potentially uniquely for this program, I'm applying with a degree in mathematics. Since I graduated, it's been clear to me that software development is the career path I want to follow. Developing video games is an even greater goal, since the game industry is one I've been following for years. The Ubisoft Graduate Program immediately struck me as a way to jumpstart my career in this industry with a company that has established a wide range of franchises, products, and ideas in its almost 34-year history.
-
-My goal has led me over the past few months to learn a broad range of skills in programming to fill in many of the gaps that my degree missed. In particular, I've gone from learning about UE4, to Unity, to graphics programming, functional and object-oriented programming, and more. Along the way, I've picked up as much as I can about data structures, algorithms, heap/stack, and other important aspects of computer science. Importantly though, the maths degree I have enables me to think abstractly about problems I encounter, and to quickly learn new ideas and methods to tackle these problems.
-
-It would be incredible to have the chance to show Ubisoft the unique skills I can bring, and I look forward to hearing back soon!
-
-// role of gameplay programmer
-A skilled gameplay programmer is able to take a gameplay idea, and using the tools available to them, stretch it as far as it will go and reward the player for experimenting with the idea and using it in imaginative ways. For example, Super Mario Bros has the core idea of running and jumping.
- 
- The tools available back when game development was still young would have been C or assembly, compared to high-level languages and visual programming languages (such as UE4's blueprints) that are available now. The ability of early developers to make fun gameplay mechanics with such limited tools is testament to their skill.
- 
-// key for game to be successful
-A successful game is one that constantly has the player saying "I want to play another level/match" or "I wonder what happens next".
-
-// game inspiration
-When I was young, my dad had a SNES and an N64 I was allowed to play on. Yoshi's Island was the game I probably spent most of my time on, replaying it over and over because the core gameplay mechanics of running, jumping, and throwing eggs was so fluid, the levels were expansive and rewarded the player for going out of their way to find hidden red coins and flowers, and the final boss music is remarkably good. A few years later, when I was about 10 or 11, I discovered GameMaker and spent almost all my time on it making platformers, inspired by my time with Yoshi's Island.
-*/
